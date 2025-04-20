d4f1e67f9393c9440f80520945615df390394bac
Kartik Malhotra <malhotrakartik5@gmail.com>
2025-04-20 15:59:24 +0530

    namste react ep-3

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-const heading = React.createElement("h1", {}, "hello")
+// const heading = React.createElement("h1", {}, "hello")
+
+const jsxHeading = <h1>Heading</h1>
+
+const Title = () => {
+    return <h2>This is title</h2>
+}
+
+const HeadingComponent = () => {
+    return (
+
+        <div><Title /><h1>Heading from component</h1></div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(heading)
+root.render(<HeadingComponent />)
